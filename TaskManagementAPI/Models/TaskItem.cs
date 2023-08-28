b029a109cade8fe7ccfa1ac3826f76a5c690ba96
jake-burton29 <jacob.burton29@gmail.com>
2023-08-27 19:10:49 -0600

    initial commit

---
+++ TaskManagementAPI/Models/TaskItem.cs
@@ -0,0 +1,12 @@
+namespace TaskManagementAPI.Models;
+
+
+public class TaskItem
+{
+    public long Id {get; set;}
+    public string? Title {get; set;}
+    public string? Description {get; set;}
+    public DateTime DueDate {get; set;}
+
+    public bool isCompleted {get; set;}
+}
